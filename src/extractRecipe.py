d826a62a9c5e3fc1ad8bd9e5b8690ba7bbbc93a4
Zack Scholl <zack.scholl@gmail.com>
2016-06-30 08:49:54 -0400

    Added more extraction

--- src/extractRecipe.py
+++ src/extractRecipe.py
@@ -12,8 +12,24 @@ nullRecipe = json.load(open('../testing/recipe_example.json', 'r'))
 text = open('../testing/sites/allrecipes.com/index.html', 'r').read()
 page = lxml.html.fromstring(text)
 recipe = copy.deepcopy(nullRecipe)
+recipe['isBasedOnUrl'] = page.xpath(
+    '//link[@id="canonicalUrl"]')[0].attrib['href'].strip()
+recipe['author'] = page.xpath(
+    '//span[@class="submitter__name"]')[0].text_content().strip()
 recipe['name'] = page.xpath(
     '//meta[@property="og:title"]')[0].attrib['content'].strip()
-print(recipe['name'])
+recipe['description'] = page.xpath(
+    '//div[@class="submitter__description"]')[0].text_content().strip().replace('"', '')
+recipeInstructions = page.xpath(
+    '//span[@class="recipe-directions__list--item"]')
+recipe['recipeInstructions'] = []
+for instruction in recipeInstructions:
+    data = instruction.text_content().strip()
+    if len(data) > 0:
+        recipe['recipeInstructions'].append(data)
+recipe['recipeYield'] = page.xpath(
+    '//meta[@id="metaRecipeServings" and @itemprop="recipeYield"]')[0].attrib['content'].strip()
+recipe['cookTime'] = page.xpath('//')
+print(json.dumps(recipe, indent=2))
 # fooScore = scores[0].xpath(
 #     ".//div")[0].text_content().split(':')[1].split('/')[0].strip()
