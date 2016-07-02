3e90b6ec81b77e8f8a30f0972c2922f53011cecd
Zack Scholl <zack.scholl@gmail.com>
2016-07-02 18:33:31 -0400

    Added analysis

---
+++ src/analyze.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+from tqdm import tqdm
+
+mypath = "../finished"
+fs = []
+for root, directories, filenames in os.walk(mypath):
+    for filename in filenames:
+        fs.append(os.path.join(root, filename))
+
+numberWithScores = 0
+for i in tqdm(range(len(fs))):
+    f = fs[i]
+    j = json.load(open(f, "r"))
+    numberWithScores += int(j['aggregateRating']['ratingValue'] !=
+                            None and float(j['aggregateRating']['ratingValue']) > 0)
+print("%2.1f%% have scores" %
+      float(100.0 * float(numberWithScores) / float(len(fs))))
