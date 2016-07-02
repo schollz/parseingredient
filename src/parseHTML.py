b76767eac148c285da8f3fce7fd360eb415ac21b
Zack Scholl <zack.scholl@gmail.com>
2016-07-02 07:37:57 -0400

    Updated parseHTML

--- src/parseHTML.py
+++ src/parseHTML.py
@@ -13,17 +13,20 @@ def main():
     for parser in availableParsers:
         if not os.path.exists('../finished/%s' % parser):
             os.makedirs('../finished/%s' % parser)
-    fs = glob.glob('../testing/sites/*/*')
-    fs = glob.glob('../testing/sites/cooks.com/*')
-    for f in fs:
-        print(f)
-        parseRecipe(f)
+
+    # # Testing purposes
+    # fs = glob.glob('../testing/sites/cooks.com/*')
+    # for f in fs:
+    #     print(f)
+    #     parseRecipe(f)
     # processFile('../testing/sites/allrecipes.com/throwoff')
-    fs = glob.glob('../testing/sites/*/*')
-    p = multiprocessing.Pool(multiprocessing.cpu_count())
-    print("Processing %d files..." % len(fs))
-    for i in tqdm(range(0, len(fs), 2 * multiprocessing.cpu_count())):
-        p.map(parseRecipe, fs[i:i + 2 * multiprocessing.cpu_count()])
+
+    # # Process all
+    # fs = glob.glob('../testing/sites/*/*')
+    # p = multiprocessing.Pool(multiprocessing.cpu_count())
+    # print("Processing %d files..." % len(fs))
+    # for i in tqdm(range(0, len(fs), 2 * multiprocessing.cpu_count())):
+    #     p.map(parseRecipe, fs[i:i + 2 * multiprocessing.cpu_count()])
 
 if __name__ == "__main__":
     main()
