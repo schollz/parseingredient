c86f5b5ac68cc89a5e23681c33a683d5d5e80945
Zack Scholl <zack.scholl@gmail.com>
2016-07-05 11:49:46 -0400

    Generate ingredient list

--- src/analyze.py
+++ src/analyze.py
@@ -5,8 +5,8 @@ from tqdm import tqdm
 
 
 os.chdir('../finished')
-# print("Generating index of files...")
-# os.system("tree -Ufai -P '*.json.it' -I '*.it.it' -o finished.index")
+print("Generating index of files...")
+os.system("tree -Ufai -P '*.json.*' -I '*.it.it' -o finished.index")
 
 print("Opening file index...")
 fs = open('finished.index', 'r').read().split('\n')
