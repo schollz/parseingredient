af104a4ddc199148fc6fdb61b97bf256e272484c
Zack Scholl <zack.scholl@gmail.com>
2016-07-05 16:38:52 -0400

    Okay

--- src/markovRecipe.py
+++ src/markovRecipe.py
@@ -2,60 +2,79 @@ import json
 import os.path
 
 import markovify
+from tqdm import tqdm
 
-ingredientList = json.load(open("../finished/ingredients.json", "r"))
+ingredientJson = json.load(open("../finished/ingredients.json", "r"))
+ingredientList = []
+for ingredient in ingredientJson:
+    if ingredientJson[ingredient] > 100:
+        ingredientList.append(ingredient)
 
 
-def getIngredient(ing):
-    # Build the model.
-    if os.path.exists("ingredients_model.json"):
-        chain_json = json.load(open("ingredients_model.json", "r"))
-        stored_chain = markovify.Chain.from_json(chain_json)
-        text_model = markovify.Text.from_chain(chain_json)
-    else:
-        # Get raw text as string.
-        with open("../finished/ingredients.txt") as f:
-            text = f.read()
-            text_model = markovify.NewlineText(text)
-            with open("ingredients_model.json", "w") as f:
-                f.write(json.dumps(text_model.chain.to_json()))
+print("Loading instructions model...")
+if os.path.exists("instructions_model.json"):
+    chain_json = json.load(open("instructions_model.json", "r"))
+    stored_chain = markovify.Chain.from_json(chain_json)
+    instructions_model = markovify.Text.from_chain(chain_json)
+else:
+    # Get raw text as string.
+    with open("../finished/instructions.txt") as f:
+        text = f.read()
+        instructions_model = markovify.NewlineText(text)
+        with open("instructions_model.json", "w") as f:
+            f.write(json.dumps(instructions_model.chain.to_json()))
+
+
+print("Loading title model...")
+if os.path.exists("title_model.json"):
+    chain_json = json.load(open("title_model.json", "r"))
+    stored_chain = markovify.Chain.from_json(chain_json)
+    title_model = markovify.Text.from_chain(chain_json)
+else:
+    # Get raw text as string.
+    with open("../finished/titles.txt") as f:
+        text = f.read()
+        title_model = markovify.NewlineText(text)
+        with open("title_model.json", "w") as f:
+            f.write(json.dumps(title_model.chain.to_json()))
+
+
+print("Loading ingredients model...")
+if os.path.exists("ingredients_model.json"):
+    chain_json = json.load(open("ingredients_model.json", "r"))
+    stored_chain = markovify.Chain.from_json(chain_json)
+    ingredients_model = markovify.Text.from_chain(chain_json)
+else:
+    # Get raw text as string.
+    with open("../finished/ingredients.txt") as f:
+        text = f.read()
+        ingredients_model = markovify.NewlineText(text)
+        with open("ingredients_model.json", "w") as f:
+            f.write(json.dumps(ingredients_model.chain.to_json()))
+
+
+def getIngredient(ing=""):
     sentence = ""
-    while ing not in sentence:
-        sentence = text_model.make_sentence().lower()
-    print(sentence)
-
-
-def getInstruction():
-    # Build the model.
-    if os.path.exists("instructions_model.json"):
-        chain_json = json.load(open("instructions_model.json", "r"))
-        stored_chain = markovify.Chain.from_json(chain_json)
-        text_model = markovify.Text.from_chain(chain_json)
+    if ing == "":
+        sentence = ingredients_model.make_sentence(tries=1).lower()
     else:
-        # Get raw text as string.
-        with open("../finished/instructions.txt") as f:
-            text = f.read()
-            text_model = markovify.NewlineText(text)
-            with open("instructions_model.json", "w") as f:
-                f.write(json.dumps(text_model.chain.to_json()))
-    sentence = text_model.make_sentence().lower()
+        while ing not in sentence:
+            sentence = ingredients_model.make_sentence(tries=1).lower()
+    return sentence
+
+
+def getInstruction(ing=""):
+    sentence = ""
+    if ing == "":
+        sentence = instructions_model.make_sentence(tries=1).lower()
+    else:
+        while ing not in sentence:
+            sentence = instructions_model.make_sentence(tries=1).lower()
     return sentence
 
 
 def getTitle():
-    # Build the model.
-    if os.path.exists("title_model.json"):
-        chain_json = json.load(open("title_model.json", "r"))
-        stored_chain = markovify.Chain.from_json(chain_json)
-        text_model = markovify.Text.from_chain(chain_json)
-    else:
-        # Get raw text as string.
-        with open("../finished/titles.txt") as f:
-            text = f.read()
-            text_model = markovify.NewlineText(text)
-            with open("title_model.json", "w") as f:
-                f.write(json.dumps(text_model.chain.to_json()))
-    sentence = text_model.make_sentence().lower()
+    sentence = title_model.make_sentence(tries=1).lower()
     return sentence
 
 
@@ -63,12 +82,40 @@ def hasIngredients(sentence):
     words = sentence.split()
     recipeIngredients = []
     for ingredient in ingredientList:
-        if ingredient in words:
+        if ingredient in words or ingredient + "s" in words:
             recipeIngredients.append(ingredient)
     return recipeIngredients
 
-foo = getTitle()
-print(foo, hasIngredients(foo))
+print("Generating titles...")
+with open("markov_titles.txt", "w") as f:
+    for i in tqdm(range(1000)):
+        f.write(getTitle() + "\n")
 
-foo = getInstruction()
-print(foo, hasIngredients(foo))
+print("Generating ingredients...")
+with open("markov_ingredients.txt", "w") as f:
+    for i in tqdm(range(1000)):
+        f.write(getIngredient() + "\n")
+
+print("Generating instructions...")
+with open("markov_instructions.txt", "w") as f:
+    for i in tqdm(range(1000)):
+        f.write(getInstruction() + "\n")
+
+# print("Generating recipe...")
+# fullIngredients = []
+#
+#
+# foo = getTitle()
+# mainIngredient = hasIngredients(foo)
+# print(foo, mainIngredient)
+# fullIngredients += mainIngredient
+#
+# for ingredient in mainIngredient:
+#     foo = getInstruction(ing=ingredient)
+#     directionIngredients = hasIngredients(foo)
+#     print(foo, directionIngredients)
+#     fullIngredients += directionIngredients
+#
+# fullIngredients = list(set(fullIngredients))
+# for ingredient in fullIngredients:
+#     print(getIngredient(ingredient))
