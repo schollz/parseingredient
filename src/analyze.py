031a7201e2421e6450f8d61de094e37d888e41fd
Zack Scholl <zack.scholl@gmail.com>
2016-07-05 11:31:47 -0400

    Generate ingredient list

--- src/analyze.py
+++ src/analyze.py
@@ -26,7 +26,7 @@ for i in tqdm(range(0, len(fs))):
     for i in range(len(j['recipeIngredientTagged'])):
         try:
             ingredientList.update(
-                [j['recipeIngredientTagged'][i]['name'].lower()])
+                [j['recipeIngredientTagged'][i]['name'].lower().replace(')', '').replace('(', '')])
         except:
             pass
     numberWithScores += int(j['aggregateRating']['ratingValue'] !=
