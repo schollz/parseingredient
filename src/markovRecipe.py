6634bf40741481b40c3d46f78abc8ca0aaa2690a
Zack Scholl <zack.scholl@gmail.com>
2016-07-06 16:45:26 -0400

    Do one at a time

--- src/markovRecipe.py
+++ src/markovRecipe.py
@@ -11,18 +11,18 @@ for ingredient in ingredientJson:
         ingredientList.append(ingredient)
 
 
-print("Loading instructions model...")
-if os.path.exists("instructions_model.json"):
-    chain_json = json.load(open("instructions_model.json", "r"))
-    stored_chain = markovify.Chain.from_json(chain_json)
-    instructions_model = markovify.Text.from_chain(chain_json)
-else:
-    # Get raw text as string.
-    with open("../finished/instructions.txt") as f:
-        text = f.read()
-        instructions_model = markovify.NewlineText(text)
-        with open("instructions_model.json", "w") as f:
-            f.write(json.dumps(instructions_model.chain.to_json()))
+# print("Loading instructions model...")
+# if os.path.exists("instructions_model.json"):
+#     chain_json = json.load(open("instructions_model.json", "r"))
+#     stored_chain = markovify.Chain.from_json(chain_json)
+#     instructions_model = markovify.Text.from_chain(chain_json)
+# else:
+#     # Get raw text as string.
+#     with open("../finished/instructions.txt") as f:
+#         text = f.read()
+#         instructions_model = markovify.NewlineText(text)
+#         with open("instructions_model.json", "w") as f:
+#             f.write(json.dumps(instructions_model.chain.to_json()))
 
 
 print("Loading title model...")
@@ -39,18 +39,18 @@ else:
             f.write(json.dumps(title_model.chain.to_json()))
 
 
-print("Loading ingredients model...")
-if os.path.exists("ingredients_model.json"):
-    chain_json = json.load(open("ingredients_model.json", "r"))
-    stored_chain = markovify.Chain.from_json(chain_json)
-    ingredients_model = markovify.Text.from_chain(chain_json)
-else:
-    # Get raw text as string.
-    with open("../finished/ingredients.txt") as f:
-        text = f.read()
-        ingredients_model = markovify.NewlineText(text)
-        with open("ingredients_model.json", "w") as f:
-            f.write(json.dumps(ingredients_model.chain.to_json()))
+# print("Loading ingredients model...")
+# if os.path.exists("ingredients_model.json"):
+#     chain_json = json.load(open("ingredients_model.json", "r"))
+#     stored_chain = markovify.Chain.from_json(chain_json)
+#     ingredients_model = markovify.Text.from_chain(chain_json)
+# else:
+#     # Get raw text as string.
+#     with open("../finished/ingredients.txt") as f:
+#         text = f.read()
+#         ingredients_model = markovify.NewlineText(text)
+#         with open("ingredients_model.json", "w") as f:
+#             f.write(json.dumps(ingredients_model.chain.to_json()))
 
 
 def getIngredient(ing=""):
@@ -73,8 +73,8 @@ def getInstruction(ing=""):
     return sentence
 
 
-def getTitle():
-    sentence = title_model.make_sentence(tries=1).lower()
+def getTitle(num):
+    sentence = title_model.make_sentence(tries=1,max_overlap_ratio=1)
     return sentence
 
 
@@ -87,19 +87,28 @@ def hasIngredients(sentence):
     return recipeIngredients
 
 print("Generating titles...")
+import time
+from multiprocessing import Pool
+t = time.time()
 with open("markov_titles.txt", "w") as f:
-    for i in tqdm(range(1000)):
-        f.write(getTitle() + "\n")
+    for i in tqdm(range(10)):
+        f.write(getTitle(i) + "\n")
+print((time.time()-t)/10.0)
 
-print("Generating ingredients...")
-with open("markov_ingredients.txt", "w") as f:
-    for i in tqdm(range(1000)):
-        f.write(getIngredient() + "\n")
+# t = time.time()
+# p = Pool(4)
+# p.map(getTitle, range(500))
+# print((time.time()-t)/500.0)
 
-print("Generating instructions...")
-with open("markov_instructions.txt", "w") as f:
-    for i in tqdm(range(1000)):
-        f.write(getInstruction() + "\n")
+# print("Generating ingredients...")
+# with open("markov_ingredients.txt", "w") as f:
+#     for i in tqdm(range(1000)):
+#         f.write(getIngredient() + "\n")
+
+# print("Generating instructions...")
+# with open("markov_instructions.txt", "w") as f:
+#     for i in tqdm(range(1000)):
+#         f.write(getInstruction() + "\n")
 
 # print("Generating recipe...")
 # fullIngredients = []
