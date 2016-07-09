0c3113bf512a2ef90793ed7dfa71aad539887c1b
Zack Scholl <zack.scholl@gmail.com>
2016-07-09 12:56:55 -0400

    Updated ingredientTagging

--- src/ingredientTagging.py
+++ src/ingredientTagging.py
@@ -22,23 +22,26 @@ def processFile(f):
 		os.mkdir(os.path.join("/tmp/"+baseName))
 	except:
 		pass
-	inputTxt = os.path.join("/tmp/"+baseName,"input.txt")
-	resultsTxt = os.path.join("/tmp/"+baseName,"results.txt")
+	try:
+		inputTxt = os.path.join("/tmp/"+baseName,"input.txt")
+		resultsTxt = os.path.join("/tmp/"+baseName,"results.txt")
 
-	with open(inputTxt,"w") as fwrite:
-		for ingredient in data['recipeIngredient']:
-			fwrite.write(ingredient.strip() + "\n")
+		with open(inputTxt,"w") as fwrite:
+			for ingredient in data['recipeIngredient']:
+				fwrite.write(ingredient.strip() + "\n")
 
-	cmd = "python /home/phi/Downloads/ingredient-phrase-tagger/lib/testing/parse-ingredients.py " + inputTxt
-	with open(resultsTxt,"w") as fwrite:
-		fwrite.write(subprocess.check_output(cmd.split(), universal_newlines=True))
+		cmd = "python /home/phi/Downloads/ingredient-phrase-tagger/lib/testing/parse-ingredients.py " + inputTxt
+		with open(resultsTxt,"w") as fwrite:
+			fwrite.write(subprocess.check_output(cmd.split(), universal_newlines=True))
 
-	cmd = "python /home/phi/Downloads/ingredient-phrase-tagger/lib/testing/convert-to-json.py " + resultsTxt
-	resultJson = json.loads(subprocess.check_output(cmd.split(), universal_newlines=True))
-	data['recipeIngredientTagged'] = copy.deepcopy(resultJson)
-	with open(f+'.it','w') as fwrite:
-		fwrite.write(json.dumps(data,indent=2))
-	shutil.rmtree(os.path.join("/tmp/"+baseName))
+		cmd = "python /home/phi/Downloads/ingredient-phrase-tagger/lib/testing/convert-to-json.py " + resultsTxt
+		resultJson = json.loads(subprocess.check_output(cmd.split(), universal_newlines=True))
+		data['recipeIngredientTagged'] = copy.deepcopy(resultJson)
+		with open(f+'.it','w') as fwrite:
+			fwrite.write(json.dumps(data,indent=2))
+		shutil.rmtree(os.path.join("/tmp/"+baseName))
+	except:
+		return
 
 print("Getting all the files...")
 fs = getAllFiles()
